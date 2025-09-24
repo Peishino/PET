48118c276744e99b0db6f43e7c9cca528cee3675
Gabriel Moreira <gabriel.moreira419@gmail.com>
2025-09-23 22:02:52 -0300

    dia 2 na luta

--- eustat/Literalmente Eustatística/analise_dos_dados.R
+++ eustat/Literalmente Eustatística/analise_dos_dados.R
@@ -1,13 +1,13 @@
-install.packages("googlesheets4")
+#install.packages("googlesheets4")
 library(googlesheets4)
 
-url <- ""
-gs4_auth(email = "", scopes = "https://www.googleapis.com/auth/spreadsheets.readonly")
+url <- "https://docs.google.com/spreadsheets/d/1ADpRfqFmci-P-psHRDWjYookIxx2_O-wczSQbAag9CM/edit?usp=sharing"
+gs4_auth(email = "gabriel.moreira419@gmail.com", scopes = "https://www.googleapis.com/auth/spreadsheets.readonly")
 medidas <- read_sheet(url, sheet = "Medidas")
 treinos <- read_sheet(url, sheet = "Treinos")
 sono <- read_sheet(url, sheet = "Sono")
 peso <- read_sheet(url, sheet = "Peso")
-
+setwd("C:/Users/bielm/OneDrive/Documents/GitHub/PET/eustat/Literalmente Eustatística")
 
 library(dplyr)
 library(lubridate)
@@ -26,11 +26,11 @@ sono <- sono %>%
       minute = minute(`Tempo dormido`),
       second = second(`Tempo dormido`)
     ),
-    Soneca = seconds_to_period(duration(
+    Soneca = duration(
       hour = hour(Soneca),
       minute = minute(Soneca),
       second = second(Soneca)
-    )),
+    ),
     Total_dormido = duration(
       hour = hour(Total_dormido),
       minute = minute(Total_dormido),
@@ -40,16 +40,19 @@ sono <- sono %>%
 
 sono <- sono %>%
   filter(`Tempo dormido` > duration(0))
-
 # Análise exploratória
 
 # Mínimo e Máximo de Tempo Dormido Geral
 minimo_tempo_dormido <- seconds_to_period(min(sono$`Tempo dormido`, na.rm = TRUE))
-maximo_tempo_dormido <- seconds_to_period(max(sono_filtrado$`Tempo dormido`, na.rm = TRUE))
+maximo_tempo_dormido <- seconds_to_period(max(sono$`Tempo dormido`, na.rm = TRUE))
 media_tempo_dormido <- seconds_to_period(mean(sono$`Tempo dormido`, na.rm = TRUE))
 
+jpeg("histograma_sono.jpg", width = 1200, height = 600)
 hist(sono$`Tempo dormido`, main = "", xlab = "", col = "lightblue", border = "black")
+dev.off()
+jpeg("boxplot_sono.jpg", width = 1200, height = 600)
 boxplot(sono$`Tempo dormido`, main = "", ylab = "", col = "lightgreen")
+dev.off()
 
 # Analisando por mês
 sono <- sono %>%
@@ -60,9 +63,10 @@ sono_mes <- sono %>%
   summarise(
     Min_Tempo_Dormido = seconds_to_period(min(`Tempo dormido`, na.rm = TRUE)),
     Max_Tempo_Dormido = seconds_to_period(max(`Tempo dormido`, na.rm = TRUE)),
-    Media_Tempo_Dormido = seconds_to_period(mean(`Tempo dormido`, na.rm = TRUE))
+    Media_Tempo_Dormido = seconds_to_period(mean(`Tempo dormido`, na.rm = TRUE)),
+    Soma_Tempo_Dormido = seconds_to_period(sum(`Tempo dormido`, na.rm = TRUE))
   )
-
+media_dias_dormidos_mes <- seconds_to_period(mean(as.numeric(sono_mes$Soma_Tempo_Dormido)))
 par(mfrow = c(1,3))
 
 barplot(as.numeric(sono_mes$Min_Tempo_Dormido),
@@ -85,9 +89,41 @@ barplot(as.numeric(sono_mes$Max_Tempo_Dormido),
         col = "lightblue",
         ylab = "Tempo Dormido (em segundos)",
         ylim = c(0, 40000))
-# fazer soma do tempo dormido por mês
-# ver se dá pra relacionar maior tempo dormido com as férias da faculdade
 
+
+# gráfico de barras com uma linha de média 
+library(ggplot2)
+graf1 <- ggplot(sono_mes, aes(x = Mês, y = as.numeric(Soma_Tempo_Dormido))) +
+  geom_bar(stat = "identity", fill = "lightgray") +
+  geom_hline(yintercept = as.numeric(media_dias_dormidos_mes), color = "#002B36", linetype = "dashed", size = 1) +
+  geom_text(aes(label = as.character(Soma_Tempo_Dormido)), vjust = 1.5) +
+  geom_text(
+    x = tail(sono_mes$Mês, 1), 
+    y = as.numeric(media_dias_dormidos_mes), 
+    label = as.character(round(media_dias_dormidos_mes, 0)),
+    color = "#002B36", # A cor do texto
+    hjust = 0.33, # Ajuste horizontal para mover o texto para fora da barra
+    vjust = -0.5 # Ajuste vertical para mover o texto para cima da linha
+  ) +
+  labs(title = "") + 
+  theme_minimal() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.title.x = element_blank()
+  )
+graf1
+ggsave(
+  filename = "grafico_tempo_dormido_mes.jpg",
+  plot = graf1,
+  width = 12,
+  height = 6,
+  units = "in",
+  dpi = 300
+)
+getwd()
 # Analisando por dia da semana
 
 sono_semana <- sono %>%
@@ -98,21 +134,6 @@ sono_semana <- sono %>%
     Media_Tempo_Dormido = seconds_to_period(mean(`Tempo dormido`, na.rm = TRUE))
   ) %>%
   arrange(Dia_semana_num)
-
-library(ggplot2)
-
-cores <- if_else(sono_semana$Dia_semana %in% c("sábado", "domingo"), "lightcoral", "lightgray")
-
-ggplot(sono_semana, aes(x = Dia_semana, y = as.numeric(Min_Tempo_Dormido))) +
-  geom_bar(stat = "identity", fill = cores) +
-  geom_text(aes(label = as.character(Min_Tempo_Dormido)), vjust = -0.5) +
-  labs(title = "Mínimo Tempo Dormido por Dia da Semana") + 
-  theme_minimal() +
-  theme(axis.title.x = element_blank())
-
-  
-
-
 
 sono_semana_ordenado <- sono_semana %>%
   mutate(Dia_semana = factor(Dia_semana,
@@ -125,10 +146,9 @@ sono_semana_ordenado <- sono_semana %>%
 cores <- if_else(sono_semana_ordenado$Dia_semana %in% c("sábado", "domingo"), "lightcoral", "lightgray")
 cores2 <- if_else(sono_semana_ordenado$Dia_semana %in% c("sábado", "domingo"), "lightgreen", "lightgray")
 
-ggplot(sono_semana_ordenado, aes(x = Dia_semana, y = as.numeric(Min_Tempo_Dormido))) +
+graf2 <- ggplot(sono_semana_ordenado, aes(x = Dia_semana, y = as.numeric(Min_Tempo_Dormido))) +
   geom_bar(stat = "identity", fill = cores) +
   geom_text(aes(label = as.character(Min_Tempo_Dormido)), vjust = -0.5) +
-  # Personalizações do tema
   theme_minimal() +
   theme(
     panel.grid.major = element_blank(),
@@ -137,20 +157,118 @@ ggplot(sono_semana_ordenado, aes(x = Dia_semana, y = as.numeric(Min_Tempo_Dormid
     axis.text.y = element_blank(),
     axis.title.x = element_blank()
   )
+ggsave(
+  filename = "menor_sono_diasemana.jpg",
+  plot = graf2,
+  width = 12,
+  height = 6,
+  units = "in",
+  dpi = 300
+)
 
-
-ggplot(sono_semana_ordenado, aes(x = Dia_semana, y = as.numeric(Max_Tempo_Dormido))) +
+graf3 <- ggplot(sono_semana_ordenado, aes(x = Dia_semana, y = as.numeric(Max_Tempo_Dormido))) +
   geom_bar(stat = "identity", fill = cores2) +
   geom_text(aes(label = as.character(Max_Tempo_Dormido)), vjust = -0.5) +
-  # Personalizações do tema
-  theme_minimal() + # Já remove grande parte das linhas de fundo
+  theme_minimal() + 
   theme(
-    # Remove as linhas de fundo (major e minor)
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
-    # Remove o título do eixo Y e os rótulos de valores
     axis.title.y = element_blank(),
     axis.text.y = element_blank(),
-    # Remove o título do eixo X
     axis.title.x = element_blank()
   )
+ggsave(
+  filename = "maior_sono_diasemana.jpg",
+  plot = graf3,
+  width = 12,
+  height = 6,
+  units = "in",
+  dpi = 300
+)
+
+# Relacionando o tempo dormido com a soneca
+
+soneca <- sono %>%
+  filter(Soneca > duration(0))
+
+minimo_soneca <- seconds_to_period(min(soneca$Soneca, na.rm = TRUE))
+maximo_soneca <- seconds_to_period(max(soneca$Soneca, na.rm = TRUE))
+media_soneca <- seconds_to_period(mean(soneca$Soneca, na.rm = TRUE))
+
+media_soneca_mes <- soneca %>%
+  group_by(Mês) %>%
+  summarise(Media_Soneca = seconds_to_period(mean(Soneca, na.rm = TRUE)))
+# histograma da soneca
+seconds_to_period(4000)
+jpeg("histograma_soneca.jpg", width = 1200, height = 600)
+hist(soneca$Soneca, main = "", xlab = "", col = "lightblue", border = "black")
+dev.off()
+# mediana
+mediana_soneca <- seconds_to_period(median(soneca$Soneca, na.rm = TRUE))
+
+# qual a correlação?
+correlacao <- cor(as.numeric(soneca$`Tempo dormido`), as.numeric(soneca$Soneca), use = "complete.obs")
+correlacao 
+
+cores_soneca <- ifelse(soneca$Soneca < duration(minutes = 40) | soneca$Soneca > duration(minutes = 250), "lightgray", "#002B36")
+
+# eu tenho costume de tirar uma soneca de aproximadamente 40 minutos por dia, vou remover isso e um outlier pra ver como fica a correlação
+
+
+
+soneca$cor <- ifelse(as.numeric(soneca$Soneca, "minutes") < 50 | as.numeric(soneca$Soneca, "minutes") > 250, "grey", "#002B36")
+
+# Gera o gráfico 1
+plot1 <- ggplot(soneca, aes(x = as.numeric(`Tempo dormido`), y = as.numeric(Soneca))) +
+  geom_point(aes(color = cor), size = 3, show.legend = FALSE) +
+  geom_smooth(method = "lm", se = FALSE, color = "lightcoral") +
+  labs(title = "") +
+  theme_minimal() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank()
+  ) +
+  scale_color_manual(values = c("#002B36" = "#002B36", "grey" = "grey"))
+
+plot1
+ggsave(
+  filename = "soneca_tempo_dormido.jpg",
+  plot = plot1,
+  width = 12,
+  height = 6,
+  units = "in",
+  dpi = 300
+)
+
+soneca_filtrada <- soneca %>%
+  filter(as.numeric(Soneca, "minutes") >= 50 & as.numeric(Soneca, "minutes") <= 250)
+correlacao_filtrada <- cor(as.numeric(soneca_filtrada$`Tempo dormido`), as.numeric(soneca_filtrada$Soneca), use = "complete.obs")
+correlacao_filtrada
+# Gera o gráfico 2
+plot2 <- ggplot(soneca_filtrada, aes(x = as.numeric(`Tempo dormido`), y = as.numeric(Soneca))) +
+  geom_point(color = "#002B36", size = 3) +
+  geom_smooth(method = "lm", se = FALSE, color = "lightcoral") +
+  labs(title = "") +
+  theme_minimal() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank()
+  )
+
+plot2
+ggsave(
+  filename = "soneca_tempo_dormido_filtrado.jpg",
+  plot = plot2,
+  width = 12,
+  height = 6,
+  units = "in",
+  dpi = 300
+)
